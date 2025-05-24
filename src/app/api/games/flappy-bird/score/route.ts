9d3432c49eae7f36530228dedb193f6063e02472
Mohamed Saci <mouhflh73@gmail.com>
2025-05-24 19:42:10 +0100

    clean endoint calls deleted definivly

--- src/app/api/games/flappy-bird/score/route.ts
+++ src/app/api/games/flappy-bird/score/route.ts
@@ -16,20 +16,6 @@ export async function POST(request: Request) {
     if (!newScore)
       return new Response("Internal Server Error", { status: 500 });
 
-    {
-      /* 
-    const cleanupResponse = await fetch("http://localhost:3000/api/games/cleanup-db", {
-      method: "GET",
-    });
-
-    const data = await cleanupResponse.json();
-
-    if (!data.success) {
-      return new Response(data.message, { status: 500 });
-    }
-    */
-    }
-
     return new Response(JSON.stringify({ newScore, success: true }), {
       status: 200,
     });
